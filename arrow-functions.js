7d7a2ae24d0a07fdb3111cf47cdb2b3197c1eed5
Dale Fielding <dalefielding28@gmail.com>
2023-11-25 21:08:15 +0000

    Add the different examples for arrow functions mentioned in the CI tutorial

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,33 @@
  */
 
 // Vanilla JavaScript Function
+  // function addTwoNumbers(a, b) {
+  //   return a + b;
+  // }
+  // let sum = addTwoNumbers(3, 5);
+  // console.log(sum);
 
 // Arrow Function With Parameters
 
+  const addTwoNumbers = (a, b) => {
+    return a + b;
+  }
+  let sum = addTwoNumbers(3, 5);
+  console.log(sum);
+
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b; // shorter syntax for returning. (a + b) is also valid.
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
 
-// Returning Multiple Lines
+// Returning Multiple Lines - the parenthesis in this implicit function are required
+const returnMultipleLines = () => ( 
+  `<p>
+  This is a multiline string
+  </p>`
+)
+console.log(returnMultipleLines());
